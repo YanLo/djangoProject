a6bbf0f2c28b361f8ea529c1d752530aff553bae
YanLo <logovoya@tut.by>
2021-10-07 09:43:15 +0300

    add templates, bootstrap and css

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import  views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about '),
+    path('about1/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Yan',
+        'title': 'post1',
+        'content': 'dummy data',
+        'date_posted': '7 Oct 2021'
+    },
+    {
+        'author': 'YanLo',
+        'title': 'post2',
+        'content': 'dummy data 2',
+        'date_posted': '9 Oct 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
