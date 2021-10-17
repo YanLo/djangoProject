76267bd5d4befb601b5d1da44c95f2cdadd53ecf
YanLo <logovoya@tut.by>
2021-10-17 09:51:07 +0300

    add users app

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Yan',
-        'title': 'post1',
-        'content': 'dummy data',
-        'date_posted': '7 Oct 2021'
-    },
-    {
-        'author': 'YanLo',
-        'title': 'post2',
-        'content': 'dummy data 2',
-        'date_posted': '9 Oct 2021'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
